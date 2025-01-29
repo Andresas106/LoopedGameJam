5d16cee792ba9a8e32ff6f90e4e21168c4d389bb
Ed3X <edgar.obrador@estudiantat.upc.edu>
2025-01-29 20:13:04 +0100

    as

--- Assets/Assets/Animations/Level Tutorial/Torus.cs
+++ Assets/Assets/Animations/Level Tutorial/Torus.cs
@@ -3,19 +3,6 @@ using UnityEngine;
 public class TorusInteractivo : MonoBehaviour, IInteractuable
 {
     public Animator objetoAnimado; // Objeto que se animará al interactuar
-    private InputManager inputManager; // Referencia al InputManager
-
-    void Start()
-    {
-        // Obtener el InputManager desde el objeto (si está en el mismo objeto)
-        inputManager = FindObjectOfType<InputManager>();
-
-        // Desactivar la habilidad de "morir" al inicio
-        if (inputManager != null)
-        {
-            inputManager.SetDiePressed(false); // Asegura que la acción de "morir" esté desactivada al principio
-        }
-    }
 
     public void Interactuar()
     {
@@ -27,12 +14,6 @@ public class TorusInteractivo : MonoBehaviour, IInteractuable
 
         // Desactiva el torus
         gameObject.SetActive(false);
-
-        // Activar la habilidad de "morir" en el InputManager
-        if (inputManager != null)
-        {
-            inputManager.SetDiePressed(true); // Activa la acción de morir
-        }
 
         Debug.Log("Torus interactuado: animación activada y torus desactivado.");
     }
--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+
     InputCharacter input;
 
     public Vector2 CurrentMovementInput { get; private set; }
@@ -11,29 +14,35 @@ public class InputManager : MonoBehaviour
     public bool IsDiePressed { get; private set; }
     public bool IsInteractPressed { get; private set; }
     public Vector2 CurrentMouseDelta { get; private set; }
-
+    // Start is called before the first frame update
     void Awake()
     {
         input = new InputCharacter();
 
+        //Esto se ejecuta cuando el personaje pulsa AWSD o el left stick del gamepad
         input.Player.Move.started += onMovementInput;
         input.Player.Move.canceled += onMovementInput;
         input.Player.Move.performed += onMovementInput;
-
+        //Esto se ejecuta cuando el personaje pulsa shift Izq o el boton del gamepad
         input.Player.Run.started += onRunInput;
         input.Player.Run.canceled += onRunInput;
-
+        //Esto se ejecuta cuando el personaje pulsa espacio o el boton del gamepad
         input.Player.Jump.started += onJumpInput;
         input.Player.Jump.canceled += onJumpInput;
 
-        input.Player.Look.performed += onLookInput;
-        input.Player.Look.canceled += onLookInput;
+        // Detectar movimiento del ratón
+        input.Player.Look.performed += onLookInput; // Nuevo evento
+        input.Player.Look.canceled += onLookInput; // Detener movimiento del ratón
 
         input.Player.Die.started += onDieInput;
         input.Player.Die.canceled += onDieInput;
 
+
         input.Player.Interact.started += onInteractInput;
         input.Player.Interact.canceled += onInteractInput;
+
+
+
     }
 
     private void onInteractInput(InputAction.CallbackContext context)
@@ -53,10 +62,11 @@ public class InputManager : MonoBehaviour
 
     private void onMovementInput(InputAction.CallbackContext context)
     {
+        //Al detectar los inputs del teclado o gamepad se tendran dos valores de x e y
         CurrentMovementInput = context.ReadValue<Vector2>();
     }
 
-    private void onLookInput(InputAction.CallbackContext context)
+    private void onLookInput(InputAction.CallbackContext context) // Método para capturar movimiento del ratón
     {
         CurrentMouseDelta = context.ReadValue<Vector2>();
     }
@@ -74,11 +84,5 @@ public class InputManager : MonoBehaviour
     private void OnDisable()
     {
         input.Player.Disable();
-    }
-
-    // Método público para cambiar el estado de IsDiePressed
-    public void SetDiePressed(bool value)
-    {
-        IsDiePressed = value;
     }
 }
